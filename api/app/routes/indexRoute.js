eeefc2cb5f6f2e63417e77ce5374764873145040
Rajan <rajansoft1@gmail.com>
2016-04-07 17:45:44 +0530

    WIP

--- api/app/routes/indexRoute.js
+++ api/app/routes/indexRoute.js
@@ -12,6 +12,7 @@ module.exports = function (app) {
     app.get('/user/:email', user.getUserByEmail);
     app.get('/activate/:token', user.activate);
     app.get('/user/:email/:token', user.updateResultToken);
+    app.get('/offline/:email/:token', user.updateResultToken);
     app.post('/user/register', user.register);
     app.post('/refer', user.refer);
     app.post('/user/login', user.login);
