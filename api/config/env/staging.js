03f23706da19f440e4dafd64d741a33f5f3c6600
rajan-hl <rajan.c@homelane.com>
2017-04-02 00:03:13 +0530

    Update staging.js

--- api/config/env/staging.js
+++ api/config/env/staging.js
@@ -1,9 +1,7 @@
 module.exports = {
     // Development configuration options
-    sessionSecret: 'intialQuoteStaging',
-    db: 'mongodb://localhost/quoteCalculatorStage',
-    getClientDetail: 'http://apps.homelane.com/webapi/customers/aG9tZWxhbmVvZmZsaW5lY2F0YWxvZw==/info?email=',
-    getClientBucket: 'http://54.169.216.87:80/v1/customers/',
+    sessionSecret: 'frfrfrc',
+    db: 'mongodb://localhost/errvr',
     log: {
         // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
         format: 'dev',
